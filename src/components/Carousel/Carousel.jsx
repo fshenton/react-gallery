5c2621d8645dde070e25b165582f18a9992cdec2
fshenton <fshenton@gmail.com>
2019-08-17 17:20:48 +0100

    Added CarouselNavButton component and included two in the Carousel return.

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { s } from "./";
 import CarouselImage from "COMPONENTS/CarouselImage/";
+import CarouselNavButton from "COMPONENTS/CarouselNavButton/";
 
 export default class Carousel extends Component {
 	render(){
 		return (
 			<output className={s.carousel}>
 				<CarouselImage />
+				<CarouselNavButton />
+				<CarouselNavButton />
 			</output>
 		);
 	}
---
+++ src/components/CarouselNavButton/CarouselNavButton.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { s } from "./";
+
+export default class CarouselNavButton extends Component {
+	render(){
+		return (
+			<button onClick={(e) => e.preventDefault()}>
+				BUTTON
+			</button>
+		);
+	}
+}
