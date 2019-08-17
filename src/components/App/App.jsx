19f7a9d4b9c6bab8fbbfbcf7ced315972b5fe76b
fshenton <fshenton@gmail.com>
2019-08-17 14:18:13 +0100

    App now imports styles via index.js.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import { s } from "./";
 
 export default class App extends Component {
 	render(){
 		return (
+			<div className={s.appWrapper}>
 				<p>Hello World</p>
+			</div>
 		);
 	}
 }
